9c035868b0191eee50f8f213790994b3837451a3
Reed <reedsilverstein@gmail.com>
2018-05-23 01:03:06 -0500

    reset added

--- sketch.js
+++ sketch.js
@@ -1,5 +1,9 @@
 var img;
 var randomVars = [];
+var runningTotal = [];
+var runningTotalSize = 15;
+var frames = 0;
+
 
 function preload() {
   img = loadImage("assets/monaLisa.jpg");
@@ -14,15 +18,20 @@ function setup() {
 }
 
 function draw() {
-  sortPixels();
+  if (frames%5 == 0)
+    sortPixels();
 
-  if (frameCount%200 == 0)
+  if (frames%500 == 0)
     reset();
+
+  frames++;
 }
 
 function sortPixels() {
-  loadPixels();
+  var changedAmount = 0;
+  var average = 0;
 
+  loadPixels();
   for(var x=0;x<width;x++) {
     for(var y=height;y>=0;y--) {
       var index = (x+y*width)*4;
@@ -38,10 +47,26 @@ function sortPixels() {
         pixels[otherIndex+randomVars[18]] = tempPixels[0]+randomVars[19];
         pixels[otherIndex+randomVars[20]] = tempPixels[1]+randomVars[21];
         pixels[otherIndex+randomVars[22]] = tempPixels[2]+randomVars[23];
+        changedAmount++;
       }
     }
   }
   updatePixels();
+
+  // Check to see if reset is needed because the image is not doing anything
+  runningTotal.push(changedAmount);
+  if (runningTotal.length > runningTotalSize) {
+    runningTotal.shift();
+  }
+  average = round(runningTotal.reduce(getSum)/runningTotal.length);
+  if (average == changedAmount) {
+    reset();
+    frames=0;
+  }
+}
+
+function getSum(total, num) {
+    return total + num;
 }
 
 function reset() {
